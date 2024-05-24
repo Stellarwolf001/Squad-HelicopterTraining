0a93727a51f6425af1bcad5783cd5aaa519b881e
Stellarwolf001 <97207291+Stellarwolf001@users.noreply.github.com>
2024-05-24 18:06:21 +0800

    Delete Squad-HelicopterTraining-main directory

--- Squad-HelicopterTraining-main/config.c
+++
@@ -1,7 +0,0 @@
-#include "./config.h"
-
-// 注意替换下面的路径为实际的 rcon.exe 文件路径
-const char *rconPath = "/home/SquadGameServer/Squad-HelicopterTraining/rcon";
-const char *serverPassword = "-PYourRCONPassword";
-const char *serverPort = "-p21114";
-const char *log_address = "/home/SquadGameServer/SquadGame/Saved/Logs/SquadGame.log";
